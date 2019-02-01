523dbb81ecf16c1332318ca555a7a58c69ea7b48
Dennis Afanasev <dennis.afa@gmail.com>
2019-02-01 16:21:42 -0500

    Updates
    First commit

---
+++ flaredetect.py
@@ -0,0 +1,118 @@
+import numpy as np
+def flaredetectpeak(flux):
+    global listFlare
+    global firstval
+    j = 0
+    listFlare = []
+
+    baseval = np.std(flux) * 2
+    #print(baseval)
+    noise = get_noise(flux, baseval)
+
+    while j < len(flux)-1:
+        if flux[j] > baseval:
+            peak = flux[j]
+            firstval = flux[j]
+            if (flux[j] - flux[j + 1]) < 0:
+                while j < len(flux) - 1 and flux[j] < flux[j + 1]:
+                    peak = flux[j + 1]
+                    j += 1
+                else:
+                    temp = flux[j]
+                    base = flux[j]
+                    h = j
+                    while h > 1 and flux[h] > flux[h - 1]:
+                        base = flux[h - 1]  # peak will be point before it rises
+                        h -= 1
+                    else:
+                        if temp - base > noise:
+                            listFlare.append(peak)
+                    j += 1
+            else:
+                temp = flux[j]
+                base = flux[j]
+                h = j
+                while h > 1 and flux[h] > flux[h-1]:
+                    base = flux[h - 1] # peak will be point before it rises
+                    h -= 1
+                else:
+                    if temp - base > noise:
+                        listFlare.append(peak)
+                j += 1
+        else:
+            j += 1
+    return listFlare
+
+
+def getlength():
+    return len(listFlare)
+
+def model_peaks(flux):
+    flux = np.asarray(flux)
+    listFlare = []
+    j = 0
+    while j < len(flux) - 1:
+        if (flux[j] - flux[j + 1]) < 0:
+           while j < len(flux) - 1 and flux[j] < flux[j + 1]:
+                j += 1
+
+           if j + 1 < len(flux):
+            listFlare.append(flux[j])
+        else:
+            j+=1
+
+    return len(listFlare)
+
+def get_std(flux):
+    noise = np.std(flux)
+    return noise
+
+def get_noise(flux, baseval):
+    list_flare = [fl for fl in flux if fl < baseval]
+    noise_check = []
+    for i, val in enumerate(list_flare):
+        if i < len(list_flare) - 1:
+            dist = np.absolute(list_flare[i] - list_flare[i+1])
+            noise_check.append(dist)
+    return np.average(noise_check)
+
+def flaredetecttime(time, flux):
+    global listFlare
+    global firstval
+    j = 0
+    listFlare = []
+
+    baseval = get_std(flux) * 2 # 2 sigma deviation
+    noise = get_noise(flux, baseval)
+    while j < len(flux)-1:
+        if flux[j] > baseval:
+            firstval = flux[j]
+            if (flux[j] - flux[j + 1]) < 0:
+                while j < len(flux) - 1 and flux[j] < flux[j + 1]:
+                    j += 1
+                else:
+                    temp = flux[j]
+                    base = flux[j]
+                    h = j
+                    while h > 1 and flux[h] > flux[h - 1]:
+                        base = flux[h - 1]  # peak will be point before it rises
+                        h -= 1
+                    else:
+                        if temp - base > noise:
+                            listFlare.append(time[j])
+                    j += 1
+            else:
+                temp = flux[j]
+                base = flux[j]
+                h = j
+                while h > 1 and flux[h] > flux[h-1]:
+                    base = flux[h - 1] # peak will be point before it rises
+                    h -= 1
+                else:
+                    if temp - base > noise:
+                        listFlare.append(time[j])
+                j += 1
+        else:
+            j += 1
+    return listFlare
+
